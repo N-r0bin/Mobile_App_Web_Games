6ceee36c144da83e43d5a257f12b813e75e7898c
N-r0bin <noham20@outlook.com>
2022-08-23 16:27:49 +0300

    Added more games to the games list

--- lib/model/games_data.dart
+++ lib/model/games_data.dart
@@ -51,6 +51,61 @@ List<Map<String, dynamic>> games_data = [
     "name": "Table Tennis World Tour",
     "url": "https://play.famobi.com/table-tennis-world-tour"
   },
+  {
+    "id": 11,
+    "name": "React Wordle",
+    "url": "https://reactle.vercel.app/"
+  },
+  {
+    "id": 12,
+    "name": "2048 Game",
+    "url": "https://play2048.co/"
+  },
+  {
+    "id": 13,
+    "name": "Clumsy Bird",
+    "url": "https://ellisonleao.github.io/clumsy-bird/"
+  },
+  {
+    "id": 14,
+    "name": "Pacman in HTML5 Canvas",
+    "url": "https://pacman.platzh1rsch.ch/"
+  },
+  {
+    "id": 15,
+    "name": "Astray",
+    "url": "https://wwwtyro.github.io/Astray/"
+  },
+  {
+    "id": 16,
+    "name": "Canvas Tetris",
+    "url": "https://dionyziz.com/graphics/canvas-tetris/"
+  },
+  {
+    "id": 17,
+    "name": "Tic Tac Toe",
+    "url": "https://codepen.io/iamrubberducky/full/XjLGKv"
+  },
+  {
+    "id": 18,
+    "name": "0hh1",
+    "url": "https://0hh1.com/"
+  },
+  {
+    "id": 19,
+    "name": "Javascript Pseudo 3D Racer",
+    "url": "https://codeincomplete.com/games/racer/"
+  },
+  {
+    "id": 20,
+    "name": "mk.js",
+    "url": "https://mk.mgechev.com/"
+  },
+  {
+    "id": 21,
+    "name": "Freeciv Web",
+    "url": "https://www.fciv.net/"
+  },
 ];
 
 final List<GamesData> allGames = games_data.map((e) => GamesData.fromJson(e)).toList();
